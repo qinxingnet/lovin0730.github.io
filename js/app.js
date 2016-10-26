b1307591801d517f88bfb9f1759af69cd237d6b1
Lovin0730 <xy610888871@gmail.com>
2016-10-26 12:03:28 +0800

    Site updated: 2016-10-26 12:03:28

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
+var customSearch;
 (function($) {
 	
 	"use strict";
-	
-	var customSearch = {};
-  
+
   var scrolltoElement = function(e) {
     e.preventDefault();
     var self = $(this),
@@ -40,14 +39,36 @@
 	  if (SEARCH_SERVICE === 'google') {
   	  customSearch = new GoogleCustomSearch({
     	  apiKey: GOOGLE_CUSTOM_SEARCH_API_KEY,
-    	  engineId: GOOGLE_CUSTOM_SEARCH_ENGINE_ID
+    	  engineId: GOOGLE_CUSTOM_SEARCH_ENGINE_ID,
+        imagePath: "/images/"
   	  });
 	  }
-	  else if (SEARCH_SERVICE === 'bing') {
-  	  customSearch = new BingSearch();
+	  else if (SEARCH_SERVICE === 'algolia') {
+  	  customSearch = new AlgoliaSearch({
+    	  apiKey: ALGOLIA_API_KEY,
+    	  appId: ALGOLIA_APP_ID,
+    	  indexName: ALGOLIA_INDEX_NAME,
+        imagePath: "/images/"
+  	  });
 	  }
 	  else if (SEARCH_SERVICE === 'hexo') {
-  	  customSearch = new HexoSearch();
+  	  customSearch = new HexoSearch({
+        imagePath: "/images/"
+      });
+	  }
+	  else if (SEARCH_SERVICE === 'azure') {
+  	  customSearch = new AzureSearch({
+    	  serviceName: AZURE_SERVICE_NAME,
+        indexName: AZURE_INDEX_NAME,
+        queryKey: AZURE_QUERY_KEY,
+        imagePath: "/images/"
+  	  });
+	  }
+	  else if (SEARCH_SERVICE === 'baidu') {
+  	  customSearch = new BaiduSearch({
+				apiId: BAIDU_API_ID,
+        imagePath: "/images/"
+			});
 	  }
 
 	});
